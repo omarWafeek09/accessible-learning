427c3ad125814a6a36d8428d4702a8739b2df32d
omarWafeek09 <omarwafeek09@gmail.com>
2026-06-08 03:54:25 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-oxc'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-})  
+  base: '/'
+})
